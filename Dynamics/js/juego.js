88ab3e16049946061d8993948e5a76883a2e6125
HatsiL-64 <324082625@alumno.enp.unam.mx>
2025-06-19 13:29:34 -0600

    Correccion a juego.js Ya funciona todo

--- Dynamics/js/juego.js
+++ Dynamics/js/juego.js
@@ -1,28 +1,29 @@
-let string = document.getElementById("texto-input");
+let string = document.getElementById("texto-inputcont");
 let boton = document.getElementById("contar");
 let pantalla = document.getElementById("result-cont");
+
 
 boton.addEventListener("click", function () {
     let texto = string.value;
     let arrPalabras = texto.split(" ");
     let charXPalabra = 0;
-
     let caracteresCEspacios = texto.length;
-    let cantidadPalabras;
-    let caracteresSEspacios;
-    let palabrasLargas;
+    let cantidadPalabras = 0;
+    let caracteresSEspacios = 0;
+    let palabrasLargas = [];
 
     arrPalabras.forEach(element => {
-        if(element != " ")
+        if(element != "")
             cantidadPalabras++;
         if(element.length >= charXPalabra)
             charXPalabra = element.length;
         caracteresSEspacios += element.length;
     });
+    
     for(let i = 0; i < arrPalabras.length; i++)
     {
-        if(arrPalabras[i].length = charXPalabra )
+        if(arrPalabras[i].length == charXPalabra )
             palabrasLargas.push(arrPalabras[i]);
     }
-    pantalla.innerHTML = `Caracteres: ${caracteresCEspacios} <br> Caracteres Sin Espacios ${caracteresSEspacios} <br> Palabras ${cantidadPalabras}`;
+    pantalla.innerHTML += `Caracteres: ${caracteresCEspacios} <br> Caracteres Sin Espacios ${caracteresSEspacios} <br> Palabras ${cantidadPalabras} <br> Palabras mas largas: ${palabrasLargas}`;
 });
